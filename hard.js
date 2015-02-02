3a242e8cd10c4a7ea0f5403a39a7f488d40023c3
Jeff Beck <jeff.kitconcepts@gmail.com>
2015-02-02 16:05:53 -0500

    completed hard.js

--- hard.js
+++ hard.js
@@ -5,23 +5,25 @@ var b = 2;
 var c = b + a;
 
 
-// Q: What is the value of c?
+// Q: What is the value of c?  23
 
 
 // Write to the best if your understanding
-// why c is the value that it is.
+// why c is the value that it is.  a is a string and b is a value; So in var c - it's joining 2 plus the string 3 to make 23.
 
 
 
 var str = "Version " + 5 + 1;
 
 
-// Q: What is the value of str?
+// Q: What is the value of str? Version 51
 
 
-// Write to the best if your understanding
+// Write to the best if your understanding -  Due to the string it will only join together
 // why str is the value that it is. How could
 // it be altered to result in 6 instead of 51?
+
+var str = "Version " + (5 + 1);
 
 
 
